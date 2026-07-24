b4ba0820ffa1f9427067df095acdd37d7b9400b9
iampeerzada <50506430+iampeerzada@users.noreply.github.com>
2026-07-25 04:12:43 +0530

    fix: update https agent configuration for connection
    Refine TLS settings to ensure compatibility and stability by disabling unauthorized certificate rejection and specifying modern cipher suites.

--- server.cjs
+++ server.cjs
@@ -405,7 +405,12 @@ async function connectToWhatsApp(instanceId) {
             auth: state,
             printQRInTerminal: false,
             browser: Browsers.macOS('Desktop'),
-            agent: new (require('https')).Agent({ family: 4 }),
+                        agent: new (require('https')).Agent({ 
+                family: 4,
+                rejectUnauthorized: false,
+                minVersion: 'TLSv1.2',
+                ciphers: 'TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384'
+            }),
             markOnlineOnConnect: false,
             connectTimeoutMs: 60000,
             defaultQueryTimeoutMs: 60000,
